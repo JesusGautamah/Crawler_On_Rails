1b325a1be78e921716dafcb994a65b7aebf0ac47
JesusGautamah <lima.jesuscc@gmail.com>
2021-10-03 06:58:58 -0300

    heroku sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,41 @@
+require 'sidekiq/web'
+require 'sidekiq/cron/web'
+
 Rails.application.routes.draw do
 
-  # configuração do sidekiq
-  require 'sidekiq/web'
+  def draw(routes_name)
+    instance_eval(File.read(Rails.root.join("config/routes/#{routes_name}.rb")))
+  end
+
+  draw :sidekiq
+
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_USERNAME"])) &
+      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_PASSWORD"]))
+  end
+  
+
+
+
   mount Sidekiq::Web => '/sidekiq'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   resources :crawled_news
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
