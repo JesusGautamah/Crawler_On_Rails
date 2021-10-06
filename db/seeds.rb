37c6406252b8b6980dfc4cee2c2ea5aaf524c767
JesusGautamah <lima.jesuscc@gmail.com>
2021-10-06 02:31:46 -0300

    Crawling from seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+unless CrawledNews.where(id: 1).exists?
 
-CrawlerJob.perform_later()
+    CrawlerJob.perform_later()
+end
